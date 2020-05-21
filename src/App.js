d4c6efb12d761a3d7a4a1d308c21f2553bc4cbcb
Alex Williams <Zizzs17@gmail.com>
2020-05-21 13:10:08 -0700

    Added Items to App State
    I have added all the items given in the prompt to the App state. Each item includes the name, the base price, if it taxed, if it has an import tax, and the image.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,62 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          name: "Book",
+          price: 12.49,
+          taxed: false,
+          import: false,
+          image:
+            "https://images.freeimages.com/images/large-previews/f7d/book-1179516.jpg",
+        },
+        {
+          name: "Music CD",
+          price: 14.99,
+          taxed: true,
+          import: false,
+          image:
+            "https://images.freeimages.com/images/large-previews/976/compact-disk-1242785.jpg",
+        },
+        {
+          name: "Chocolate Bar",
+          price: 0.85,
+          taxed: false,
+          import: false,
+          image:
+            "https://images.freeimages.com/images/large-previews/cb6/chocolate-1322112.jpg",
+        },
+        {
+          name: "Packet of Headache Pills",
+          price: 9.75,
+          taxed: false,
+          import: false,
+          image:
+            "https://images.freeimages.com/images/large-previews/154/pills-1421720.jpg",
+        },
+        {
+          name: "Imported Box of Chocolates",
+          price: 10.0,
+          taxed: true,
+          import: true,
+          image:
+            "https://images.freeimages.com/images/large-previews/b39/chocolates-1186984.jpg",
+        },
+        {
+          name: "Imported Bottle of Perfume",
+          price: 47.5,
+          taxed: true,
+          import: true,
+          image:
+            "https://images.freeimages.com/images/large-previews/6d0/perfume-1560440.jpg",
+        },
+      ],
+      cart: [],
+    };
+  }
   render() {
     return (
       <div>

6ed8107f713f0bc3ef6f0e46b06b3b5aeada561d
Alex Williams <Zizzs17@gmail.com>
2020-05-21 13:54:07 -0700

    Added Materialize UI
    Added the Materialize UI npm package, and changed the "Add Item to Cart" button to the relevant icon from the package to test the icon usage.

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -6,7 +6,7 @@ function Item(props) {
       <p>{props.item.name}</p>
       <img src={props.item.image} alt={props.item.name} />
       <p>{props.item.price}</p>
-      <button>Add To Cart</button>
+      <i class="small material-icons">add_shopping_cart</i>
     </div>
   );
 }
